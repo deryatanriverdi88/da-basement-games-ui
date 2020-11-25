c3f5ae810bffcaf68127d7bcbbce437e5404c883
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-11-24 23:55:12 -0500

    imports React, useEffect from react, withRouter from react-router-dom, connect from react-redux, Routes, style.css an d NavBar within App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from 'react';
+import Routes from './Routes'
+import {withRouter} from 'react-router-dom'
+import './STYLES/style.css'
+import {connect} from 'react-redux'
+import NavBar from './Components/NavBar'
+
+function App(props) {
+  useEffect(() => {
+    fetch('https://da-basement-games-api.herokuapp.com/favorite_cards')
+    .then(res => res.json())
+    .then(cardItems => {
+      console.log(cardItems)
+    })
+  }, [props])
 
 function App() {
   return (
