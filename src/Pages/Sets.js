91c62fcf48c7b0380a8eba4ceeb5ee410fc64522
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-11-27 23:51:14 -0500

    adds useEffect function to Sets

--- src/Pages/Sets.js
+++ src/Pages/Sets.js
@@ -7,6 +7,33 @@ import ActiveCard from '../Components/ActiveCard'
 
 function Sets(props) {
     const location = useLocation()
+    useEffect(() => {
+        if(location.state){
+            if(location.state.setCards && location.state.setCards.length > 0){
+                props.setGroupsCards(location.state.setCards)
+            }else if (location.state.card && location.state.card.id){
+                props.setActiveCard(location.state.card)
+                props.setGroupsCards(location.state.set)
+            }
+        }else if(!location.state){
+            let path = location.pathname.split('/')
+            path.shift()
+            if(path.length === 2){
+                fetch(`https://da-basement-games-api.herokuapp.com/cards?setName=${path[1]}`)
+                .then(res=>res.json())
+                .then(cards => {
+                    props.setGroupsCards(cards)
+                })
+            }else if(path.length === 3){
+                fetch(`https://da-basement-games-api.herokuapp.com/cards?setName=${path[1]}`)
+                .then(res=>res.json())
+                .then(cards => {
+                    props.setGroupsCards(cards)
+                    props.setActiveCard(cards.find(card => card.name === path[2]))
+                })
+            }
+        }
+    }, [])
     return (
         <div>
         </div>
