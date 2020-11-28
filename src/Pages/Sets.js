61d68fc97fede9f7497f504de114124b44082c6e
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-11-27 23:49:12 -0500

    imports SetItem, SetCards and ActiveCards components

--- src/Pages/Sets.js
+++ src/Pages/Sets.js
@@ -1,6 +1,9 @@
 import {useEffect} from 'react'
 import { connect } from 'react-redux'
 import {withRouter, useLocation} from 'react-router-dom'
+import SetItem from '../Components/SetItem'
+import SetCards from '../Components/SetCards'
+import ActiveCard from '../Components/ActiveCard'
 
 function Sets() {
     return (
