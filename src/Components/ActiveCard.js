35ef201df0a1747dbb88f00d103bf139e18d4a0f
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-11-28 00:05:46 -0500

    adds img tag to render image of the card within ActiveCard

--- src/Components/ActiveCard.js
+++ src/Components/ActiveCard.js
@@ -1,6 +1,12 @@
 export default function ActiveCard({card}) {
     return (
-        <div>
-        </div>
+        <>
+            {
+                card.id ?
+                <img src={card.img_url} alt={card.name} style={{width: "300px", height: "400px"}}/>
+                :
+                null
+            }
+        </>
     )
 }
