8535ba72458154ca894cd3f227c0c4ad2831fd33
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-11-28 23:12:44 -0500

    destractures props in ActiveCard

--- src/Components/ActiveCard.js
+++ src/Components/ActiveCard.js
@@ -1,4 +1,5 @@
 export default function ActiveCard({card}) {
+    const { name, img_url, normal_low_price, normal_mid_price, normal_high_price, normal_market_price, foil_low_price, foil_mid_price, foil_high_price, foil_market_price, group_name, rarity } = card
     return (
         <>
             {
